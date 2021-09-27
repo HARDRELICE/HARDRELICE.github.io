aa1478e1bd36ba4746d22e55737a1b6c07d54453
HARDRELICE <60246839+HARDRELICE@users.noreply.github.com>
2021-09-27 18:16:36 +0800

    Site updated: 2021-09-27 18:16:36

---
+++ js/adsense.js
@@ -0,0 +1,5 @@
+(function(){
+	let s = document.createElement("script");
+	document.body.appendChild(s);
+	s.outerHTML = '<script data-ad-client="ca-pub-6144149244740247" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>';
+})()
